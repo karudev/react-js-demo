1313d9a8212a50f9019e240baa320731be7bef03
Dolyveen RENAULT <renault@karudev.fr>
2018-02-18 01:26:53 +0100

    phones page

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,37 +1,56 @@
 import React, {Component} from 'react';
 import store from './../Store/MyStore'
 import { connect } from 'react-redux';
-import ReactTimeout from 'react-timeout'
+import ReactTimeout from 'react-timeout';
+import InputRange from 'react-input-range';
 
 const API = 'http://api.dev';
 
 class Search extends Component{
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            capacity : {min : 16 , max: 1024},
+            price : {min : 0 , max: 1000}
+        };
+    }
 
 
     search(){
+
 
         var suffix = "";
         var name = document.getElementsByName("name")[0].value;
         var state = document.getElementsByName("state")[0].value;
         var operatingSystem = document.getElementsByName("operatingSystem")[0].value;
+        var capacityMin = document.getElementsByName("capacityMin")[0].value;
+        var capacityMax = document.getElementsByName("capacityMax")[0].value;
+        var priceMin = document.getElementsByName("priceMin")[0].value;
+        var priceMax = document.getElementsByName("priceMax")[0].value;
 
+
+        suffix +="?storageCapacity[between]="+capacityMin+".."+capacityMax;
+        suffix +="&price[between]="+priceMin+".."+priceMax;
 
         if(name !== "") {
-            suffix += "name=" + name;
+            suffix += "&name=" + name;
         }
 
         if(state !== "") {
-            suffix += (suffix != "" ? "&" : "")+"state=" + state;
+            suffix += "&state=" + state;
         }
 
         if(operatingSystem !== "") {
-            suffix += (suffix != "" ? "&" : "")+"operatingSystem=" + operatingSystem;
+            suffix += "&operatingSystem=" + operatingSystem;
         }
 
 
 
-        fetch(API + '/phones.json?'+suffix)
+
+
+        fetch(API + '/phones.json'+suffix)
             .then((resp) => resp.json())
             .then(function(data){
                 store.dispatch({
@@ -49,7 +68,7 @@ class Search extends Component{
 
                 <div className="col-md-12 form-group text-left">
                     <label>Recherche</label>
-                    <input name="name" onBlur={this.search}  placeholder="Rechercher un téléphone (Nom, Capacité, Prix...)" className="form-control col-md-12 input-sm"/>
+                    <input name="name" onBlur={this.search}  placeholder="Rechercher un téléphone" className="form-control col-md-12 input-sm"/>
                 </div>
 
                 <div className="col-md-12 text-left form-group">
@@ -69,6 +88,35 @@ class Search extends Component{
                             <option value="occasion">Occasion</option>
                         </select>
                 </div>
+
+                <div className="col-md-12 text-left form-group">
+                    <label>Capacité</label>
+                    <InputRange
+                        name="capacity"
+                        allowSameValues = {false}
+                        formatLabel={capacity => `${capacity} Go`}
+                        value={this.state.capacity}
+                        onChange={capacity => this.setState({ capacity })}
+                        onChangeComplete={this.search}
+                        maxValue={1024}
+                        minValue={16}
+                    />
+                </div>
+
+                <div className="col-md-12 text-left form-group">
+                    <label>Prix</label>
+                    <InputRange
+                        name="price"
+                        allowSameValues = {false}
+                        formatLabel={price => `${price} €`}
+                        value={this.state.price}
+                        onChange={price => this.setState({ price })}
+                        onChangeComplete={this.search}
+                        maxValue={1000}
+                        minValue={0}
+                    />
+                </div>
+
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-input-range/lib/css/index.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
